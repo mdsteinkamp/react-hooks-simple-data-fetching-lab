eaebb76d30bff3c229c914209ede7dd8dede6cf6
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-03-12 18:33:52 -0700

    commit tree 9d2985594e0ae44c32bc9de95ddea7f53952a074

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App () {
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [image, setImage] = useState(null)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data)
+                setImage(data.message)
+                setIsLoaded(true)
+            })
+        
+    }, []) 
+
+    if (!isLoaded) {
+        return <h3>Loading...</h3>
+    }
+    else {
+        return (
+
+        <div>
+            <h1>hello</h1>
+            <img src={image} alt="A Random Dog" />
+
+        </div>
+    )}
+}
+
+export default App
